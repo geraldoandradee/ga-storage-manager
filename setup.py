a7217d43274f3c3a303b46536ecf05512997afcb
Geraldo Andrade <geraldo@geraldoandrade.com>
2018-08-24 00:06:09 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='storage_manager',
+    version='1.0.0',
+    packages=['storage_manager'],
+    url='https://github.com/geraldoandradee/storage-manager',
+    license='MIT',
+    author='geraldoandradee',
+    author_email='geraldo@geraldoandrade.com',
+    description=''
+)
