1066d697327d60a7b54ca6d0724d9d019362bea7
Geraldo Andrade <geraldo@geraldoandrade.com>
2018-08-26 17:04:52 -0300

    Save and List methods tested. Changed package name

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,7 +1,7 @@
 import os
 import unittest2
 
-from storage_manager import StorageManager, StorageManagerEngineException, StorageManagerDataException
+from ga_storage_manager import StorageManager, StorageManagerEngineException, StorageManagerDataException
 
 
 class TestStorageManager(unittest2.TestCase):
@@ -46,11 +46,41 @@ class TestStorageManager(unittest2.TestCase):
 
         instance.save({"id": 1, "title": "Go python go!"})
 
+        test1 = instance.get(1)
+        self.assertEqual(test1["id"], 1, msg="Test1 must have an id equals to '1'")
+        self.assertEqual(test1["title"], "Go python go!", msg="Test1 must have an title equals to 'Go python go!'")
+
         class Test(object):
             pass
 
         t = Test()
-        t.title = "Go python go!"
+        t.title = "Go python go!!!!!!"
         t.id = 2
 
-        # instance.save(t, Test)
+        instance.save(t)
+
+        test = instance.get(2, Test)
+
+        self.assertEqual(test.id, 2, msg="Data id must be 2")
+        self.assertEqual(test.title, "Go python go!!!!!!")
+
+    def test_storage_manager_list(self):
+        instance = StorageManager(storage_manager_engine=self.storage_manager_engine,
+                                  storage_manager_file_path=self.storage_manager_file_path)
+
+        self.assertTrue("list" in dir(instance), msg="Storage manager must have list method")
+
+        instance.save({"id": 77, "name": "Geralt of Rivia", "job": "Witcher"})
+
+        data = instance.list()
+        self.assertTrue(isinstance(data, list), msg="The list method must return a list type")
+
+        for item in data:
+            if item["id"] == 77:
+                self.assertEqual(item["id"], 77)
+                self.assertEqual(item["name"], "Geralt of Rivia")
+                self.assertEqual(item["job"], "Witcher")
+
+                return
+
+        self.fail("Data about Geralt of Rivia was not found.")
